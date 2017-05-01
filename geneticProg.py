6e44808bbf5d7da030f8190b155823528f4c85a2
adamhursey <adam_hursey@taylor.edu>
2017-04-30 21:26:31 -0400

    Add files via upload

--- geneticProg.py
+++ geneticProg.py
@@ -1,6 +1,9 @@
 import random
 import sys
 
+
+fullList = []
+studentList = [[1,1,0,1,1],[1,1,1,0,1],[0,0,1,0,1],[0,1,0,1,0],[1,0,1,1,1],[0,1,1,1,0]]
 def createSequence():
 	seq = []
 	for i in range (4):
@@ -11,6 +14,32 @@ def createSequence():
 			seq.append('#')
 		if randNum == 2 :
 			seq.append('F')
-	print seq
-	#sys.stdout.flush()
-createSequence()
+	return seq
+
+def ruleTesting(studentSequence,test):
+	count = 0
+	score = 0
+	for i in test:
+		if i != '#':
+			if studentSequence.index(count) == 0 and i == 'F':
+				score +=1
+			if studentSequence.index(count) == 1 and i == 'T':
+				score +=1
+			else:
+				score -=1
+		else:
+			score+=1
+		
+		count+=1
+	if studentSequence.index(4)==1 and score>2:
+		return true
+	else:
+		return false
+
+
+def testing():
+	testingList = []
+	for i in range (10):
+		testingList.append(createSequence())
+
+
